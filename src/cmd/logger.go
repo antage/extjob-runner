ba6dcb7a1dcc754cc29b2e27661695064d65c689
Anton Ageev <antage@gmail.com>
2013-05-12 21:50:14 +0400

    Don't truncate log file

--- src/cmd/logger.go
+++ src/cmd/logger.go
@@ -12,7 +12,7 @@ func reopenLogger() {
 	if len(*logFilename) == 0 {
 		logger = log.New(os.Stderr, "", log.LstdFlags)
 	} else {
-		file, err := os.Create(*logFilename)
+		file, err := os.OpenFile(*logFilename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.FileMode(0640))
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Can't open log file: %s\n", err.Error())
 			os.Exit(1)
